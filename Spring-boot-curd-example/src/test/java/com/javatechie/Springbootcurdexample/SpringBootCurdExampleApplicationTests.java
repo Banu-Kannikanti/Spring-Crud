6b4e9a78083a4d5bc048c75e34ae7dc414ccbc70
JavaDeveloper23 <banupoojak@gmail.com>
2022-01-10 18:55:45 +0530

    second commit

--- Spring-boot-curd-example/src/test/java/com/javatechie/Springbootcurdexample/SpringBootCurdExampleApplicationTests.java
+++ Spring-boot-curd-example/src/test/java/com/javatechie/Springbootcurdexample/SpringBootCurdExampleApplicationTests.java
@@ -1,9 +1,7 @@
 package com.javatechie.Springbootcurdexample;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
